9238b364545eb403340e6ef221f3c4b571524e8b
Faijaz <faijaz321@gmail.com>
2023-03-01 10:12:50 +0530

    accounts session fully completed

--- OwnShoppi/urls.py
+++ OwnShoppi/urls.py
@@ -20,8 +20,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls'))
     path('cart/',include('cart.urls')),
+    path('accounts/',include('accounts.urls')),
     path('',include('shop.urls')),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
+
+def register(request):
+    if request.method=='POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email =request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"Username already taken")
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"email already taken")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password1)
+                user.save()
+                return redirect('/')
+        else:
+            messages.info(request,"please enter same password")
+            return redirect('register')
+    else:
+        return render(request,'register.html')
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,"User and password did not match ")
+            return redirect('login_page')
+    else:
+        return render(request,'login_page.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
